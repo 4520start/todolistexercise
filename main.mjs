2b9029a41985a3c046d4ecd712f15f832aa19c32
inoue-shinichi <srso4.ino@gmail.com>
2024-07-09 17:39:51 +0900

    各種項目追加

--- main.mjs
+++ main.mjs
@@ -18,6 +18,8 @@ app.get("/", async (request, response) => {
         (todo) => `
           <li>
             <span>${escapeHTML(todo.title)}</span>
+            <span>期限：${todo.due.toLocaleDateString('ja-JP',{year: 'numeric', month: '2-digit', day: '2-digit'})}</span>
+            <span>作成日：${todo.date.toLocaleDateString('ja-JP',{year: 'numeric', month: '2-digit', day: '2-digit'})}</span>
             <form method="post" action="/delete" class="delete-form">
               <input type="hidden" name="id" value="${todo.id}" />
               <button type="submit">削除</button>
@@ -31,8 +33,27 @@ app.get("/", async (request, response) => {
 });
 
 app.post("/create", async (request, response) => {
+  const date = new Date();
+  let title = request.body.title;
+  const dueDay = new Date(request.body.due);
+
+  // 優先度に応じてタイトルを修正
+  switch (request.body.priority) {
+    case 'high':
+      title = `<span style="color: red;">${escapeHTML(title)}</span>`;
+      break;
+    case 'medium':
+      title = `<span style="color: orange;">${escapeHTML(title)}</span>`;
+      break;
+    case 'low':
+      title = `<span style="color: green;">${escapeHTML(title)}</span>`;
+      break;
+    default:
+      title = escapeHTML(title);
+      break;
+  }
   await prisma.todo.create({
-    data: { title: request.body.title },
+    data: { title: title, due: dueDay, date: date, priority: request.body.priority },
   });
   response.redirect("/");
 });
